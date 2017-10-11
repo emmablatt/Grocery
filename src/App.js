eef18fd5c897b629940a24870ceba98a8de6c5c9
emmablatt <elblatt@uwaterloo.ca>
2017-10-11 08:45:23 -0400

    Refactor a lickle

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import { addItem } from './actions'
 import { connect } from 'react-redux'
 import ItemsContainer from './ItemsContainer'
 import AddItem from './AddItem'
-import { TabNavigator } from 'react-navigation'
 
-class GroceryScreen extends Component {
+export default class App extends Component {
   render() {
     return (
       <View>
@@ -17,28 +16,3 @@ class GroceryScreen extends Component {
     )
   }
 }
-
-class PantryScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Text style={{ paddingTop: 50 }}>Pantry List</Text>
-        <AddItem />
-        <ItemsContainer />
-      </View>
-    )
-  }
-}
-
-const MainScreenNavigator = TabNavigator({
-  Grocery: { screen: GroceryScreen },
-  Pantry: { screen: PantryScreen },
-})
-
-const mapDispatchToProps = () => {
-  return {
-    addItem: addItem,
-  }
-}
-
-export default connect(mapDispatchToProps)(GroceryScreen)
